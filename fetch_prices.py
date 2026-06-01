b95e3b7eb46c478d465775bd5caa28a7497aba3d
dhavalsolanki24 <52747715+dhavalsolanki24@users.noreply.github.com>
2026-06-01 21:16:28 +0530

    Update fetch_prices.py

--- fetch_prices.py
+++ fetch_prices.py
@@ -3,6 +3,13 @@ import json
 from datetime import datetime
 from lxml import html
 import traceback
+import pytz
+
+ist = pytz.timezone("Asia/Kolkata")
+hour = datetime.now(ist).hour
+
+if hour < 6 or hour > 10:
+    exit()
 
 URL = "https://www.e2necc.com/home/eggprice"
 
